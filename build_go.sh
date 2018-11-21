4da38da2ac084a2bba8babb4c786bb36a00aa49a
Filip Dimitrov <fdimitrov@Filips-MacBook-Pro.local>
2018-11-21 15:05:20 +0200

    cross compile for Linux, MacOS and Windows

--- build_go.sh
+++ build_go.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
 go get -u -v github.com/petems/terraform-provider-extip
-go install github.com/petems/terraform-provider-extip/
-cp ~/go/bin/terraform-provider-extip /vagrant/ && echo "Success: Module built. You can run 'vagrant destroy' now"
+
+for GOOS in darwin linux windows; do
+    echo "Building $GOOS-amd64"
+    export GOOS=$GOOS
+    go build -o $GOPATH/bin/$GOOS\_amd64/terraform-provider-extip github.com/petems/terraform-provider-extip
+done
+
+mkdir -p /vagrant/.terraform.d/plugins
+
+cp -R ~/go/bin/* /vagrant/.terraform.d/plugins/ && echo "Success: Module(s) built. You can run 'vagrant destroy' now"
 
 sudo poweroff
